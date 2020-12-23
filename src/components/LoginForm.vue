63442095f3c196351fd47f641bfaa6f0197c1abc
李宇帆 <yfli@travelsky.com>
2020-12-23 18:32:15 +0800

    用vue3实现的简单登录注册功能

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,108 @@
+<template>
+  <el-form
+    ref="loginForm"
+    :model="loginUser"
+    :rules="rules"
+    label-width="100px"
+    class="loginForm sign-in-form signin-signup-login "
+  >
+    <el-form-item label="邮箱" prop="email">
+      <el-input
+        v-model="loginUser.email"
+        placeholder="Enter Email..."
+      ></el-input>
+    </el-form-item>
+    <el-form-item label="密码" prop="password">
+      <el-input
+        v-model="loginUser.password"
+        type="password"
+        placeholder="Enter Password..."
+      ></el-input>
+    </el-form-item>
+
+    <el-form-item>
+      <el-button
+        @click="handleLogin('loginForm')"
+        type="primary"
+        class="submit-btn"
+        >提交</el-button
+      >
+    </el-form-item>
+
+    <!-- 找回密码 -->
+    <div class="tiparea">
+      <p>忘记密码？ <a>立即找回</a></p>
+    </div>
+  </el-form>
+</template>
+
+<script lang="ts">
+import { ref, getCurrentInstance } from "vue";
+export default {
+  props: {
+    loginUser: {
+      type: Object,
+      required: true,
+    },
+    rules: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup() {
+    // @ts-ignore
+    const { ctx } = getCurrentInstance();
+
+    // 触发登录方法
+    const handleLogin = (formName: string) => {
+      ctx.$refs[formName].validate((valid: boolean) => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    };
+
+    return { handleLogin };
+  },
+};
+</script>
+<style scoped>
+/* form */
+.loginForm {
+  margin-top: 20px;
+  background-color: #fff;
+  padding: 20px 40px 20px 20px;
+  border-radius: 5px;
+  box-shadow: 0px 5px 10px #cccc;
+}
+
+.signin-signup-login {
+   transform: translate(-50%, -30%);
+}
+
+.submit-btn {
+  width: 100%;
+}
+.tiparea {
+  text-align: right;
+  font-size: 12px;
+  color: #333;
+}
+.tiparea p a {
+  color: #409eff;
+}
+
+/* 控制login & register显示 */
+form {
+  padding: 0rem 5rem;
+  transition: all 0.2s 0.7s;
+  overflow: hidden;
+}
+
+form.sign-in-form {
+  z-index: 2;
+}
+</style>
